cc8e3c575f3e87076dd26471b3213cb38cd617b7
hardikravaiya <118045346+hardikravaiya@users.noreply.github.com>
2022-11-24 13:19:03 +0530

    Rename calulatelength.py to 12.calulatelength.py

---
+++ python assignment/module 2/12.calulatelength.py
@@ -0,0 +1,3 @@
+hello =input("Enter any sentance:")
+
+print("The length of string is:",len(hello))
--- python assignment/module 2/calulatelength.py
+++
@@ -1,3 +0,0 @@
-hello =input("Enter any sentance:")
-
-print("The length of string is:",len(hello))
