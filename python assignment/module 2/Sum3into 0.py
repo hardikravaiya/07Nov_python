3a9926f75f9e7695094215a34b66153c97213718
hardikravaiya <118045346+hardikravaiya@users.noreply.github.com>
2022-11-24 12:21:32 +0530

    Add files via upload

---
+++ python assignment/module 2/Sum3into 0.py
@@ -0,0 +1,9 @@
+a =int(input("Enter the first num: "))
+b =int(input("Enter the second num: "))
+c =int(input("Enter the therd num: "))
+if a==b or b==c or a==c:
+    sum=0
+    print("sum of zero.",sum)
+else:
+    sum=a+b+c
+    print("the sum of three numbers is:",sum)
