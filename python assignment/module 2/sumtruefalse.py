4bbce772489c5be006763b0e131c0847d85da092
hardikravaiya <118045346+hardikravaiya@users.noreply.github.com>
2022-11-24 13:00:26 +0530

    Add files via upload

---
+++ python assignment/module 2/sumtruefalse.py
@@ -0,0 +1,10 @@
+ab=int(input("Enter value of A."))
+cd=int(input("Enter value of B."))
+
+
+if ab==cd or abs(ab-cd)==5 or (ab+cd)==5:
+    print("sum of two num is Trure.")
+    #  return True
+else:
+    print("false")
+    #  return False
