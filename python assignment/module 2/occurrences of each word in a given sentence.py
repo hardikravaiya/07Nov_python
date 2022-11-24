0caaff54157f4dc30bb88b3d03052bcd44e62315
hardikravaiya <118045346+hardikravaiya@users.noreply.github.com>
2022-11-24 15:44:55 +0530

    Add files via upload

---
+++ python assignment/module 2/occurrences of each word in a given sentence.py
@@ -0,0 +1,10 @@
+string= "my name is hardik"
+
+count=0
+
+for i in string:
+    if i != " ":
+        count = count + 1
+
+print ("String words count without space is ",count)
+print ("String words count without space is ",len(string))
