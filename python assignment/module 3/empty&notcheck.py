825140805de3dfe6f075c8a7e9102ebed7bbbccb
hardikravaiya <118045346+hardikravaiya@users.noreply.github.com>
2022-11-30 17:50:59 +0530

    Add files via upload

---
+++ python assignment/module 3/empty&notcheck.py
@@ -0,0 +1,13 @@
+lis1 = []
+if lis1!=[]:
+    print("The list is not empty")
+else:
+    print("Empty List")
+
+list1=['abc','bca','sda']
+list2=[1]
+
+if list2!=[]:
+    print("the list is not empty")
+else:
+    print("empty list")
