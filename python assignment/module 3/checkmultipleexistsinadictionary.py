c24d7d5c5c1912f0ff02637d9b3a5143a970bfb8
hardikravaiya <118045346+hardikravaiya@users.noreply.github.com>
2022-12-03 17:08:49 +0530

    Add files via upload

---
+++ python assignment/module 3/checkmultipleexistsinadictionary.py
@@ -0,0 +1,7 @@
+list = { 'name': 'haresh', 'class': 'vii','roll_id': '4'}
+
+print(list.keys() >= {'class', 'name'})
+
+print(list.keys() >= {'name', 'haresh'})
+
+print(list.keys() >= {'roll_id', 'name'})
