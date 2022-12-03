be594ef515cdf391743fdfc8f14280b9c51bed71
hardikravaiya <118045346+hardikravaiya@users.noreply.github.com>
2022-12-03 15:39:02 +0530

    Add files via upload

---
+++ python assignment/module 3/akeyordictionary.py
@@ -0,0 +1,13 @@
+abcd={'a':100,'h':200,'c':300,'d':400}
+if 'a' in abcd:
+    print("yes")
+else:
+    print("no")
+print(abcd['a'])
+
+if 'e' in abcd:
+    print("yes")
+else:
+    print("no")
+
+abcd=int(input("enter your key:"))
