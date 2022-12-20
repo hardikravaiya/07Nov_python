bf0e6e487aa99f0fa0cba33832137187612439a9
hardikravaiya <118045346+hardikravaiya@users.noreply.github.com>
2022-12-20 11:55:16 +0530

    Add files via upload

---
+++ python assignment/module 4/26.Explain Inheritance in Python with an example What is init Or What Is A Constructor In Python.py
@@ -0,0 +1,18 @@
+# Explain Inheritance in Python with an example?
+# --> Inheritance means which is use the property of other class.
+
+class Python :
+
+    Python_Student = int(input ("Enter Python's Students :"))
+    Java_Student = int (input("Enter Java's Student :"))
+
+
+class Inheritance (Python) :
+    
+    def readdata (self):
+        print ("Python Student is :",self.Python_Student)
+        print ("Java Student is :",self.Java_Student)
+
+In = Inheritance ()
+
+In.readdata()
